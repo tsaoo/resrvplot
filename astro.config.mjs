2cbff8dbc0c629d534b4274646b9a52bf50e1a12
Pieski <tinesharp@outlook.com>
2024-09-13 15:52:52 +0800

    Astro workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "http://tsaoo.github.io",
+  base: "resrvplot",
   trailingSlash: "always",
   integrations: [
     tailwind(),
